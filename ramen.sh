5e58588e769594574b856a522475c341b129db5e
Lydia <lydia.etherington@gmail.com>
2019-09-05 14:45:38 +0900

    Ramen

--- ramen.sh
+++ ramen.sh
@@ -12,24 +12,24 @@ echo "Type the number of the flavour you would like:"
 read flavour
 
 if [ $flavour=1 ]; then
-        let flavourprice=600
-	let flavourname==Salt
+        flavourprice=600
+	flavourname=Salt
 fi
 if [ $flavour=2 ]; then
-        let flavourprice==800
-	let flavourname=Picanto-Beef
+        flavourprice=800
+	flavourname=Picanto-Beef
 fi
 if [ $flavour=3 ]; then
-        let flavourprice=800
-	let flavourname==Chili-Tomato
+        flavourprice=800
+	flavourname=Chili-Tomato
 fi
 if [ $flavour=4 ]; then
-        let flavourprice=1000
-	let flavourname==Vegetable
+        flavourprice=1000
+	flavourname=Vegetable
 fi
 if [ $flavour=5 ]; then
-        let flavourprice=1200
-	let flavourname==Tantanmen
+        flavourprice=1200
+	flavourname=Tantanmen
 fi
 echo -n "You have selected $flavourname ."
 echo "Price: $flavourprice yen."
